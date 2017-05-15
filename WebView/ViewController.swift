12e47e77303c818a2069e02be1e4e642076d64c5
Rebeca Souza <rebeca@rebeca-2.local>
2017-05-15 18:43:46 -0400

    Testing webkit view

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Defines the dimentions manually
+        //webView = WKWebView(frame: CGRect(x: 0, y: 20, width: 300, height: 300))
+        //Constructor without frame
+        webView = WKWebView()
+        view.addSubview(webView)
+        //Loads html content directly into the webView
+        //webView.loadHTMLString("<h1>Html Header</h1>", baseURL: <#T##URL?#>)
+        //Loads an html file
+        //let url: URL = Bundle.main.url(forResource: "page", withExtension: "html")!
+        //Loads a page from url
+        let url: URL = URL(string: "https://www.google.com")!
+        let request: URLRequest = URLRequest(url: url)
+        webView.load(request)
+        
+        //AutoLayout override frame 
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        let width = NSLayoutConstraint(item: webView, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1.0, constant: 0 )
+        let height = NSLayoutConstraint(item: webView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1.0, constant: -20)
+        let top = NSLayoutConstraint(item: webView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 20)
+        
+        view.addConstraints([width, height, top])
+        
     }
 
     override func didReceiveMemoryWarning() {
